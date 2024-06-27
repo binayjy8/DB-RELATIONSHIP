a5c8218829ed9322690aafc4fdcc8817cf2fe373
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-06-27 11:26:17 +0530

    backend code

--- Models/user.js
+++ Models/user.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
-main().catch(err => console.log(err));
+main()
+.then(() => console.log("connection successfull"))
+.catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+  await mongoose.connect("mongodb://127.0.0.1:27017/relationDemo");
 }
