fc2d49929e552563d45130897f26818ba6aa89e3
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-06-27 15:01:07 +0530

    backend code

--- Models/cestomer.js
+++ Models/cestomer.js
@@ -9,13 +9,7 @@ async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/relationDemo");
 }
 
-const userSchema = new Schema ({
-    username : String,
-    addresses : [
-        {
-            _id: false,
-            location: String,
-            city: String,
-        },
-    ],
+const orderSchema = new Schema ({
+   item: String,
+   price: Number
 });
